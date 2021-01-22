a3403cf364f68612f170fc7a09cd6ce36edd555a
AustineGwa <austinegwa64@gmail.com>
2021-01-22 09:46:04 -0800

    main

---
+++ src/main/java/com/geeks/ApiFootballConsumer/ApiFootballConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.geeks.ApiFootballConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFootballConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFootballConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geeks/ApiFootballConsumer/ApiFootballConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geeks.ApiFootballConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFootballConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
